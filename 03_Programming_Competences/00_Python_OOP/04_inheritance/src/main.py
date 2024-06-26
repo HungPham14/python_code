2644d8e9269b3ac7ebb526f6ab8bea47ccb18dee
Hung Pham <96170868+HungPham14@users.noreply.github.com>
2024-06-26 23:52:34 +0700

    Add files via upload

--- 03_Programming_Competences/00_Python_OOP/04_inheritance/src/main.py
+++ 03_Programming_Competences/00_Python_OOP/04_inheritance/src/main.py
@@ -1,1 +1,13 @@
 '''Python Inheritance'''
+class Employee:
+    '''Base class'''
+    def __init__(self, id, name: str):
+        self.id = id
+        self.name = name
+
+# Here we declare that the Salary class inherits from the Employee class
+class Salary(Employee):
+    '''Salary is a derive class from base class Employee'''
+    def __init__(self, id, name, salary):
+        super().__init__(id, name) # super() function allows we to call the superclass | baseclass's methods
+        self.salary = salary
